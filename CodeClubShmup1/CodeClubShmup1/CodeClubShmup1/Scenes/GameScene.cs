e7c48766bca333f458d2d07cfc13f02816333dab
Tristanix <lauriz.alatalo@gmail.com>
2013-12-02 16:20:35 +0200

    initial commit

---
+++ CodeClubShmup1/CodeClubShmup1/CodeClubShmup1/Scenes/GameScene.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CodeClubShmup1.Components;
+using CodeClubShmup1.Engine;
+using Microsoft.Xna.Framework;
+using CodeClubShmup1.Objects;
+using Microsoft.Xna.Framework.Input;
+
+namespace CodeClubShmup1.Scenes
+{
+    class GameScene : SceneParent
+    {
+        ScrollingBackground background1;
+        ScrollingBackground background2;
+
+        List<Bullet> bullets = new List<Bullet>();
+        List<Enemy> enemies = new List<Enemy>();
+
+        Random random = new Random();
+        Timer enemy_spawn_timer;
+
+        Player player;
+
+        public GameScene()
+            : base()
+        {
+            enemy_spawn_timer = new Timer(3000);
+            player = new Player(Resources.GetTexture("Ship"), new Vector2(100, 100), 5);
+            background1 = new ScrollingBackground(new Vector2(-100, -100), 40, new Sprite(Resources.GetTexture("StarWars")));
+            background2 = new ScrollingBackground(new Vector2(0, 0), 30, new Sprite(Resources.GetTexture("StarWars")));
+
+        }
+
+        public override void Update(float dt)
+        {
+            base.Update(dt);
+
+            if (Input.IsKeyPressed(Keys.M))
+            {
+                SceneSys.PauseCurrentScene(true);
+                SceneSys.OpenScene(new MenuScene());
+            }
+
+
+            background1.Update(dt);
+            background2.Update(dt);
+
+            if (Input.IsKeyPressed(Keys.Space) && !player.IsDead)
+                bullets.Add(new Bullet(Resources.GetTexture("Bullet"), player.Position, 500));
+
+            if (enemy_spawn_timer.Update(dt))
+            {
+                enemies.Add(new Enemy(Resources.GetTexture("Enemy"),
+                    new Vector2(Game1.screen_size.Width,
+                        random.Next(Game1.screen_size.Height)),
+                        100));
+
+                if (enemy_spawn_timer.Delay>=200)
+                    enemy_spawn_timer.Delay -= 100;
+            }
+
+            // Game Objects Updates
+            if (!player.IsDead)
+                player.Update(dt);
+
+            foreach (Bullet item in bullets)
+            {
+                item.Update(dt);
+
+                foreach (Enemy e in enemies)
+                {
+                    if (item.CollisionRect.Intersects(e.CollisionRect)) {
+                        e.IsDead = true;
+                        item.IsDead = true;
+                    }
+                }
+
+                if (item.Position.X > Game1.screen_size.Width) {
+                    item.IsDead = true;
+                }
+            }
+
+            foreach (Enemy item in enemies)
+            {
+                item.Update(dt);
+
+                if (item.Position.X < 0)
+                {
+                    item.IsDead = true;
+                }
+
+                if (!player.IsDead)
+                {
+                    if (item.CollisionRect.Intersects(player.CollisionRect))
+                    {
+                        player.IsDead = true;
+                        item.IsDead = true;
+                    }
+                }
+            }
+
+            //deleting objects
+            for (int i = 0; i < enemies.Count; i++) {
+                Enemy e = enemies[i];
+
+                if (e.IsDead) {
+                    enemies.Remove(e);
+                    i--;
+                }
+            }
+
+            for (int i = 0; i < bullets.Count; i++)
+            {
+                Bullet e = bullets[i];
+
+                if (e.IsDead)
+                {
+                    bullets.Remove(e);
+                    i--;
+                }
+            }
+
+            //ruudun leveys
+            //screen_size.Width;
+
+            // TODO: Add your update logic here
+            
+        }
+
+        public override void Draw()
+        {
+            //spr.Draw(Vector2.One * 100);
+            //täällä piirretään kaikki mitä ruudulle halutaan
+
+            background1.Draw();
+            background2.Draw();
+
+            DrawSys.DrawText("ASLKDGHASJDGASJKDG", Resources.GetFont("CSfont"), new Vector2(100, 100), Color.LimeGreen);
+
+            if (!player.IsDead)
+                player.Draw();
+
+            foreach (Bullet item in bullets)
+            {
+                item.Draw();
+            }
+            foreach (Enemy item in enemies)
+            {
+                item.Draw();
+            }
+        }
+    }
+}
---
+++ CodeClubShmup1/CodeClubShmup1/CodeClubShmup1/Scenes/MenuScene.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CodeClubShmup1.Components;
+using CodeClubShmup1.Engine;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using CodeClubShmup1.Objects;
+
+namespace CodeClubShmup1.Scenes
+{
+    class MenuScene : SceneParent
+    {
+        Button button1;
+
+        public MenuScene()
+            : base()
+        {
+           button1 = new Button(Resources.GetTexture("Enemy"),
+               new Vector2(100,100));
+           button1.OnButtonPressed += onButton1Press;
+        }
+
+        public override void Update(float dt)
+        {
+            base.Update(dt);
+
+            button1.Update(dt);
+
+            if (Input.IsKeyPressed(Keys.M))
+            {
+                SceneSys.CloseCurrentScene();
+                SceneSys.PauseCurrentScene(false);
+            }
+        }
+
+        public override void Draw()
+        {
+            DrawSys.DrawText("GAEM TITLE", Resources.GetFont("CSfont"),
+                new Vector2 (100,100),Color.Red);
+
+            button1.Draw();
+        }
+
+        void onButton1Press()
+        {
+            Console.WriteLine("Nyt painetaan nappulaa.");
+        }
+    }
+}
